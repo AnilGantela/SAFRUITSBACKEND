69f53e369d46a81a5894ae2aa73efd78782217c8
unknown <anilkumar.gantela77@gmail.com>
2026-01-10 20:33:34 +0530

    kk

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 /* -------------------- CORS -------------------- */
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://safruitsweb.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
