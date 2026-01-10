712e23fdd64811e5d47bfb1824c6da286340d402
unknown <anilkumar.gantela77@gmail.com>
2026-01-10 23:45:10 +0530

    kk

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -79,4 +79,13 @@ const createCategory = async (req, res) => {
   }
 };
 
-module.exports = { createProduct, createCategory };
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+module.exports = { createProduct, createCategory, getAllProducts };
--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -2,8 +2,10 @@ const productRouter = require("express").Router();
 const {
   createProduct,
   createCategory,
+  getAllProducts,
 } = require("../controllers/ProductController");
 
 productRouter.post("/create", createProduct);
+productRouter.get("/", getAllProducts);
 productRouter.post("/category/create", createCategory);
 module.exports = productRouter;
